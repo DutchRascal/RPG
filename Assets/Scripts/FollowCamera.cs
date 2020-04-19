705cf16594de1649ee912bdc034defa731e3275f
DutchRascal <DutchRascal@fritz.box>
2020-04-19 16:13:34 +0200

    RPG-015

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = target.position;
+        }
+        else
+        {
+            Debug.LogWarning("FOLLOWCAMERA UPDATE: no target!");
+        }
+    }
+}
