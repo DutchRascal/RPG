27a763a2736a6759274b2379ca75c461a5d98bdc
DutchRascal <DutchRascal@fritz.box>
2020-04-19 15:46:09 +0200

    RPG-14

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,32 +9,34 @@ public class Mover : MonoBehaviour
     [SerializeField]
     Transform target;
     NavMeshAgent agent;
-    float agentSpeed;
-    Ray lastRay;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agentSpeed = agent.speed * 2;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (agent && target)
+        if (agent)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                agent.destination = target.position;
-                agent.speed = agentSpeed;
+                MoveToCursor();
             }
         }
         else
         {
             Debug.LogWarning("MOVER: Update: No agent or target");
         }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            agent.destination = hit.point;
+        }
     }
 }
