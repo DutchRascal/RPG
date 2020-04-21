73bfafb075d99d26d6cd6c2b9ee4558040f725c9
DutchRascal <DutchRascal@fritz.box>
2020-04-21 17:04:11 +0200

    RPG-018

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -8,7 +8,7 @@ public class FollowCamera : MonoBehaviour
     [SerializeField]
     Transform target;
 
-    void Update()
+    private void LateUpdate()
     {
         if (target)
         {
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -17,7 +17,7 @@ public class Mover : MonoBehaviour
     {
         if (agent)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
             }
