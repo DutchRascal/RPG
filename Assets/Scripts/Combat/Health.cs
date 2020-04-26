63fe4676adde427b0548c05688ace458dad93a0b
DutchRascal <DutchRascal@fritz.box>
2020-04-26 17:38:44 +0200

    RPG-040

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -6,10 +7,24 @@ namespace RPG.Combat
     {
         [SerializeField] float health = 100f;
 
+        bool isDead = false;
+
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(0, health - damage);
-            print(health);
+            if (health == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (!isDead)
+            {
+                GetComponent<Animator>().SetTrigger("die");
+                isDead = true;
+            }
         }
     }
 }
