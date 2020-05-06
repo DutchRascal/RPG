af5893e037246e089ce3c8b7f9538e829a76e203
DutchRascal <DutchRascal@fritz.box>
2020-05-06 17:22:25 +0200

    RPG-065

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool alreadyTriggered = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player" && !alreadyTriggered)
+            {
+                alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
