db58d6f8d1a88c8621cf5484914411d603a2a9c4
DutchRascal <DutchRascal@fritz.box>
2020-07-01 13:31:22 +0200

    RPG-155

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -3,6 +3,7 @@ using RPG.Combat;
 using RPG.Movement;
 using RPG.Resources;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace RPG.Control
 {
@@ -17,7 +18,8 @@ namespace RPG.Control
         {
             None,
             Movement,
-            Combat
+            Combat,
+            UI
         }
 
         [System.Serializable]
@@ -44,10 +46,25 @@ namespace RPG.Control
 
         void Update()
         {
-            if (health.IsDead()) { return; }
+            if (InteractWithUI()) { return; }
+            if (health.IsDead())
+            {
+                SetCursor(CursorType.None);
+                return;
+            }
             if (InteractWithCombat()) { return; }
             if (InteractWithMovement()) { return; }
             SetCursor(CursorType.None);
+        }
+
+        private bool InteractWithUI()
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                SetCursor(CursorType.UI);
+                return true;
+            }
+            return false;
         }
 
         private bool InteractWithCombat()
